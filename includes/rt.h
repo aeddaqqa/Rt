15fd0673d3aea2bfd0f14bd5cfaef46845834641
Nawfal Abouzaher <nabouzah@e2r4p5.1337.ma>
2021-02-19 17:42:49 +0100

    mlx.h

--- includes/rt.h
+++ includes/rt.h
@@ -6,7 +6,7 @@
 /*   By: nabouzah <nabouzah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 20:15:10 by aeddaqqa          #+#    #+#             */
-/*   Updated: 2021/02/10 19:28:29 by nabouzah         ###   ########.fr       */
+/*   Updated: 2021/02/19 17:42:25 by nabouzah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <fcntl.h>
 # include <stdbool.h>
 # include <math.h>
-# include "../mlx/mlx.h"
+# include "mlx.h"
 # include "../libft/libft.h"
 # include "./define.h"
 # include "./struct.h"
