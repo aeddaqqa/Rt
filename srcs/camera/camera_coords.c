4775966c4445906410b070e44100afb292f08424
Nawfal Abouzaher <nabouzah@e2r2p1.1337.ma>
2021-02-22 17:34:49 +0100

    camera bug fix

--- srcs/camera/camera_coords.c
+++ srcs/camera/camera_coords.c
@@ -6,7 +6,7 @@
 /*   By: nabouzah <nabouzah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 18:33:59 by nabouzah          #+#    #+#             */
-/*   Updated: 2021/02/21 11:04:34 by nabouzah         ###   ########.fr       */
+/*   Updated: 2021/02/22 17:32:57 by nabouzah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void		cam_cord_system(t_cam *cam)
 
 	cam->ratio = (double)W / H;
 	cam->up = (t_vect3){0.0, 1.0, 0.0};
-	cam->plan_h = tan(cam->fov * M_PI / (180.0 / 2));
-	cam->up.x = 0.00005;
+	cam->plan_h = tan((cam->fov * (M_PI /180.0)) / 2.0);
+	cam->o.x = 0.00005;
 	cam->plan_w = cam->plan_h * cam->ratio;
 	tmp = vect_sub(cam->l, cam->o);
 	cam->cords.w = normalize(tmp);
 	tmp = cross(cam->up, cam->cords.w);
 	cam->cords.u = normalize(tmp);
-	cam->cords.v = cam->up;
+	cam->cords.v = cross(cam->cords.w, cam->cords.u);
 }
