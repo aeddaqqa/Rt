bf77e6f9740c13a8aa23bc3861e4dfbe4d22f322
Charaf eddine Zabakh <chzabakh@e2r6p8.1337.ma>
2021-03-06 17:25:37 +0100

    norminette main

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,56 @@
 /*   By: aeddaqqa <aeddaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 23:16:14 by aeddaqqa          #+#    #+#             */
-/*   Updated: 2021/03/05 09:48:26 by aeddaqqa         ###   ########.fr       */
+/*   Updated: 2021/03/06 17:25:07 by chzabakh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rt.h"
 
-int	main(int ac, char **av)
+void	syntax_err(t_rt *rt, char *file)
+{
+	destroy(SYNTAX_ERROR);
+	free_rt(&rt);
+	free(file);
+	exit(0);
+}
+
+void	err(int ac, char **av, t_rt *rt, char *file)
+{
+	char *ex;
+
+	ex = ft_strrchr(av[1], '.');
+	if (!ex)
+		exit(0);
+	if (!(rt = init_rt(ac - 2)))
+		destroy(MALLOC_ERROR);
+	if (!ft_strcmp(ex, ".xml"))
+	{
+		if (!(file = load_file(av[1])))
+			exit(0);
+		if (!(parse(file, rt)))
+			syntax_err(rt, file);
+	}
+	else if (!ft_strcmp(".obj", ex))
+	{
+		if (!(parse_obj(rt, av[1])))
+			exit(0);
+	}
+	else
+		exit(EXIT_FAILURE);
+	rt->sdl = init_sdl();
+	new_camera(rt);
+	if (rt->sdl)
+		rtrace(rt);
+}
+
+int		main(int ac, char **av)
 {
 	char *file;
 	t_rt *rt;
-	char *ex;
 
 	file = NULL;
+	rt = NULL;
 	if (ac == 2 || ac == 3)
 	{
 		if (ac == 3 && ft_strcmp(av[2], "--save"))
@@ -26,34 +63,7 @@ int	main(int ac, char **av)
 			destroy(FLAG_SAVE);
 			return (0);
 		}
-		ex = ft_strrchr(av[1], '.');
-		if (!ex)
-			exit(0);
-		if (!(rt = init_rt(ac - 2)))
-			destroy(MALLOC_ERROR);
-		if (!ft_strcmp(ex, ".xml"))
-		{
-			if (!(file = load_file(av[1])))
-				exit(0);
-			if (!(parse(file, rt)))
-			{
-				destroy(SYNTAX_ERROR);
-				free_rt(&rt);
-				free(file);
-				exit(0);
-			}
-		}
-		else if (!ft_strcmp(".obj", ex))
-		{
-			if (!(parse_obj(rt, av[1])))
-				exit(0);
-		}
-		else
-			exit(EXIT_FAILURE);
-		rt->sdl = init_sdl();
-		new_camera(rt);
-		if (rt->sdl)
-			rtrace(rt);
+		err(ac, av, rt, file);
 	}
 	else
 		ft_putendl("./rt [fileName]");
