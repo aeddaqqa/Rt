52d99af93e6cf1dc630138f0b9e2d95ef8ea4bb2
Nawfal Abouzaher <nabouzah@e2r2p3.1337.ma>
2021-03-07 18:41:46 +0100

    norm

---
+++ srcs/light_effects/direct_light.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   direct_light.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nabouzah <nabouzah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/07 18:36:51 by nabouzah          #+#    #+#             */
+/*   Updated: 2021/03/07 18:41:18 by nabouzah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/rt.h"
+
+static t_color			paint(t_ray r, t_light *l, t_vect3 l_dir, t_vect3 tin)
+{
+	double	dist;
+
+	dist = ft_magnitude(v_c_prod(r.direction, tin.x));
+	if (tin.x > 0.0 && ((ft_magnitude(l_dir) < dist)) &&\
+	(dot(r.direction, l->direct_dir)) < 0)
+	{
+		return (fraction(l->color, tin.y *\
+			powf(dot(r.direction, l->direct_dir), 50)));
+	}
+	return ((t_color){0.0, 0.0, 0.0});
+}
+
+static double			cast(t_rt *rt, t_ray r, t_light *l, double *ints)
+{
+	t_object	*obj;
+	t_object	o;
+	double		t;
+
+	obj = rt->objects;
+	*ints = l->intensity;
+	t = -1.0;
+	while (obj)
+	{
+		copy_obj(&o, obj);
+		if ((t = rt->intersection[obj->type](&o, &r)) > 0.0 && !o.is_transp)
+			break ;
+		else if (t > 0 && o.is_transp)
+			*ints *= o.is_transp;
+		obj = obj->next;
+	}
+	return (t);
+}
+
+t_color					is_direct_light(t_rt *rt, t_ray r, double t)
+{
+	t_vect3		l_dir;
+	double		ints;
+	t_light		l;
+	t_light		*light;
+	t_color		blind;
+
+	blind = (t_color){0.0f, 0.0f, 0.0f};
+	light = rt->lights;
+	while (rt->direct && light)
+	{
+		l = *light;
+		l.direct_dir =\
+		normalize(vect_sub((t_vect3){0, 0, 0}, l.position));
+		l_dir = vect_sub(l.position, rt->cameras->o);
+		t = cast(rt, r, &l, &ints);
+		blind = add_color(blind, paint(r, &l, l_dir, (t_vect3){t, ints, 0}));
+		light = light->next;
+	}
+	return (blind);
+}
--- srcs/light_effects/lights.c
+++ srcs/light_effects/lights.c
@@ -6,69 +6,13 @@
 /*   By: nabouzah <nabouzah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 05:10:29 by nabouzah          #+#    #+#             */
-/*   Updated: 2021/03/07 18:20:03 by nabouzah         ###   ########.fr       */
+/*   Updated: 2021/03/07 18:38:07 by nabouzah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/rt.h"
 
-t_color		paint(t_ray r, t_light *l, t_vect3 l_dir, t_vect3 tin)
-{
-	double	dist;
-
-	dist = ft_magnitude(v_c_prod(r.direction, tin.x));
-	if (tin.x > 0.0 && ((ft_magnitude(l_dir) < dist)) &&\
-	(dot(r.direction, l->direct_dir)) < 0)
-	{
-		return (fraction(l->color, tin.y *\
-			powf(dot(r.direction, l->direct_dir), 20)));
-	}
-	return ((t_color){0.0, 0.0, 0.0});
-}
-
-double		cast(t_rt *rt, t_ray r, t_light *l, double *ints)
-{
-	t_object	*obj;
-	t_object	o;
-	double		t;
-
-	obj = rt->objects;
-	*ints = l->intensity;
-	t = -1.0;
-	while (obj)
-	{
-		copy_obj(&o, obj);
-		if ((t = rt->intersection[obj->type](&o, &r)) > 0.0 && !o.is_transp)
-			break ;
-		else if (t > 0 && o.is_transp)
-			*ints *= o.is_transp;
-		obj = obj->next;
-	}
-	return (t);
-}
-
-t_color		is_direct_light(t_rt *rt, t_ray r, double t)
-{
-	t_vect3		l_dir;
-	double		ints;
-	t_light		*l;
-	t_color		blind;
-
-	blind = (t_color){0.0f, 0.0f, 0.0f};
-	l = rt->lights;
-	while (rt->direct && l)
-	{
-		l->direct_dir =\
-		normalize(vect_sub((t_vect3){0, 0, 0}, l->position));
-		l_dir = vect_sub(l->position, rt->cameras->o);
-		t = cast(rt, r, l, &ints);
-		blind = add_color(blind, paint(r, l, l_dir, (t_vect3){t, ints, 0}));
-		l = l->next;
-	}
-	return (blind);
-}
-
-int			parallel_light(t_rt *rt, t_ray r, t_light light)
+int				parallel_light(t_rt *rt, t_ray r, t_light light)
 {
 	t_vect3		light_p;
 	double		dott;
@@ -88,7 +32,7 @@ int			parallel_light(t_rt *rt, t_ray r, t_light light)
 	return (1);
 }
 
-t_color		shade(t_rt *rt, t_light li, t_object obj, t_ray r)
+t_color			shade(t_rt *rt, t_light li, t_object obj, t_ray r)
 {
 	double	n_l;
 	int		shadow;
@@ -115,34 +59,30 @@ unsigned int	light_effect(t_rt *rt, t_object *o, t_ray *ray)
 {
 	t_color			color[rt->nbr_lights];
 	unsigned int	i;
-	t_color			tmp_res[2];
+	t_color			f_color;
 	t_light			*lights;
-	t_object		object;
 	t_light			li;
 
 	i = 0;
 	lights = rt->lights;
-	copy_obj(&object, o);
-	tmp_res[1] = ambient(&object, rt->ambient);
+	f_color = ambient(o, rt->ambient);
 	while (lights)
 	{
 		li.intensity = lights->intensity;
 		li.color = lights->color;
 		li.position = lights->position;
-		tmp_res[0] = (t_color){0.0, 0.0, 0.0};
 		li.direction = vect_sub(li.position, ray->hit_point);
 		li.d = sqrtf(dot(li.direction, li.direction));
 		li.direction = normalize(li.direction);
-		color[i++] = shade(rt, li, object, *ray);
+		color[i++] = shade(rt, li, *o, *ray);
 		lights = lights->next;
 	}
 	while (i > 0)
-		tmp_res[1] = add_color(color[--i], tmp_res[1]);
-	return (rgb(tmp_res[1]));
+		f_color = add_color(color[--i], f_color);
+	return (rgb(f_color));
 }
 
-
-int		light(t_object *close_obj, t_ray *ray, t_rt *rt, double t)
+int				light(t_object *close_obj, t_ray *ray, t_rt *rt, double t)
 {
 	ray->hit_point = vect_add(ray->origin, v_c_prod(ray->direction, t));
 	ray->t = t;
